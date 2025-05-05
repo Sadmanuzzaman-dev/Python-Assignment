144c91c09905eb9a094d462a8ed4bece66111c54
Sadmanuzzaman-dev <sadmanuzzaman.78@gmail.com>
2025-05-05 22:00:42 +0600

    first commit

---
+++ Class_1_HW.py
@@ -0,0 +1,22 @@
+user_name = input("What is your name: ")
+user_age = input("What about your age: ")
+user_roll = input("Tell me your roll: ")
+bangla_mark = int(input("Your marks in bangla: "))
+english_mark = int(input("Your marks in english: "))
+math_mark = int(input("Your marks in math: "))
+
+print("  ")
+print("Student Report:")
+print("-----------------------")
+
+print("Name: ", user_name)
+print("Age: ", user_age)
+print("Roll Number: ", user_roll)
+print("Marks in Bangla: ", bangla_mark)
+print("Marks in English: ", english_mark)
+print("Marks in Math: ", math_mark)
+
+total_marks = bangla_mark + english_mark + math_mark
+
+print("Your total marks is: ",total_marks)
+print("Your average marks is: ", total_marks/3)
